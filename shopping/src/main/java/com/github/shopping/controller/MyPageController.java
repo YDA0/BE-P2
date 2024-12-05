288d6fe55ba31a084b9de3488c4c80cd8b129017
LEEHOSUNG <ho4728@gmail.com>
2024-12-06 05:35:19 +0900

    장바구니 불러오기 추가
    myInfoController에 장바구니 목록을 불러오는 로직을 추가

--- shopping/src/main/java/com/github/shopping/controller/MyPageController.java
+++ shopping/src/main/java/com/github/shopping/controller/MyPageController.java
@@ -1,23 +1,27 @@
 package com.github.shopping.controller;
 
+import com.github.shopping.dto.CartItemDto;
 import com.github.shopping.dto.UserInfoDto;
+import com.github.shopping.service.CartService;
 import com.github.shopping.service.MyPageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/mypage")
 @Slf4j
+@RequiredArgsConstructor
 public class MyPageController {
 
     private final MyPageService myPageService;
+    private final CartService cartService;
 
-    public MyPageController(MyPageService myPageService) {
-        this.myPageService = myPageService;
-    }
 
     // 이메일로 내 정보 조회 API
     @GetMapping("/info")  // "
@@ -28,4 +32,13 @@ public class MyPageController {
         // 조회된 정보 반환
         return ResponseEntity.ok(userInfo);
     }
+
+
+    @GetMapping("/cartList")
+    public ResponseEntity<List<CartItemDto>> getCartItems() {
+        List<CartItemDto> cartItems = cartService.getCartItems(); // CartService에서 장바구니 아이템 가져오기
+        return ResponseEntity.ok(cartItems);
+    }
+
+
 }
